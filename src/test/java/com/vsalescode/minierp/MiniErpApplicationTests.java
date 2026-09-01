52a3a1d456591d39c88578aad074de4126449d9e
vsalescode <vsalescode@gmail.com>
2026-08-31 21:57:41 -0300

    backend: initialize Spring Boot foundation

---
+++ src/test/java/com/vsalescode/minierp/MiniErpApplicationTests.java
@@ -0,0 +1,15 @@
+package com.vsalescode.minierp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class MiniErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
